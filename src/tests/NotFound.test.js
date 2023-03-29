932338d9ac7f2277711dea746b5b26e9e667583c
Bruna Loyola <brunloyola02@gmail.com>
2023-03-28 21:40:27 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundTitle = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found', level: 2 },
+    );
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    renderWithRouter(<NotFound />);
+
+    const imagem = screen.getByRole('img');
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
